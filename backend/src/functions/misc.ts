aba635c9f6110519bc7cfafdd9b4098199ea00db
KTrain5369 <me@git.ktrain5369.dev>
2026-01-08 22:37:58 +1000

    add get cookie action

--- backend/src/functions/misc.ts
+++ backend/src/functions/misc.ts
@@ -1,5 +1,22 @@
-import type { ActionData, RCEAction } from "../types/rce";
+import type { ActionData, ActionResult, RCEAction } from "../types/rce";
 
-export const miscActions: RCEAction[] = []
+export const miscActions: RCEAction[] = [
+    {
+        name: 'get_cookie',
+        description: 'Get a cookie! You can even choose the flavor.',
+        schema: {
+            type: 'object',
+            properties: {
+                flavor: {
+                    type: 'string'
+                }
+            }
+        },
+        handler: handleGetCookie,
+    }
+]
 
-export function handleAskForCookie(actionData: ActionData) {}
+export async function handleGetCookie(actionData: ActionData<{ flavor?: string } | undefined>): Promise<ActionResult> {
+    const flavor = actionData.params?.flavor ?? 'test';
+    return { success: true, message: `You got a ${flavor} cookie!` }
+}
